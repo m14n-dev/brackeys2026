34c5e1268beae7820de4f17de64dbddf591efc11
Alder <blackthorn.alder@gmail.com>
2026-08-29 17:21:50 +0200

    Made some variables in base_power.gd auto-referenced

--- trust-no-one/special_powers/base_power.gd
+++ trust-no-one/special_powers/base_power.gd
@@ -1,8 +1,6 @@
 class_name BasePower
 extends Node2D
 
-@export var button: Button
-@export var audio_player: AudioStreamPlayer2D
 @export var select_audio: AudioStreamMP3
 @export var unselect_audio: AudioStreamMP3
 
@@ -10,7 +8,7 @@ var currently_selected: bool = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	button.pressed.connect(on_this_button_pressed)
+	$Button.pressed.connect(on_this_button_pressed)
 	SignalBus.button_pressed.connect(on_any_button_pressed)
 
 func on_this_button_pressed():
@@ -18,11 +16,11 @@ func on_this_button_pressed():
 	
 	if(!currently_selected):
 		select()
-		audio_player.stream = select_audio
+		$AudioStreamPlayer2D.stream = select_audio
 	else:
 		unselect()
-		audio_player.stream = unselect_audio
-	audio_player.play()
+		$AudioStreamPlayer2D.stream = unselect_audio
+	$AudioStreamPlayer2D.play()
 
 func on_any_button_pressed(source):
 	if(source != self && currently_selected):
@@ -34,5 +32,5 @@ func select():
 
 func unselect():
 	currently_selected = false
-	button.button_pressed = false;
+	$Button.button_pressed = false;
 	#TODO: animation and such feedback stuff
